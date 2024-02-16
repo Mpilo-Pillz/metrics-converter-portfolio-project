ccd969cfd571e8c66fd5404632025cf1bd912080
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-02-16 23:51:22 +0200

    wireing the picker and assigning datasource and delegate

--- UnitifyConverter/UnitifyConverter/ConverterViewController.swift
+++ UnitifyConverter/UnitifyConverter/ConverterViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
-
+    
+    let measurementTypes = ["Mass", "Length", "Consumption"]
+    
+    @IBOutlet weak var measurementTypesPicker: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        measurementTypesPicker.dataSource = self
+        measurementTypesPicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
+   
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension ConverterViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return measurementTypes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return measurementTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       print("TODO implement")
+        print(measurementTypes[row])
+    }
 }
