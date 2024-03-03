ed87b59ae37966462204571adb44cdc7ca0addd0
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-03-03 09:14:20 +0200

    creating reusable picker view

--- UnitifyConverter/UnitifyConverter/PickerViewHandler.swift
+++ UnitifyConverter/UnitifyConverter/PickerViewHandler.swift
@@ -5,4 +5,35 @@
 //  Created by Mpilo Pillz on 2024/03/03.
 //
 
-import Foundation
+import UIKit
+
+protocol PickerViewHandlerDelegate: AnyObject {
+    func didSelectITem(_ handler: PickerViewHandler, selectedITem: String)
+}
+
+class PickerViewHandler: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+    private let items: [String]
+    weak var delegate: PickerViewHandlerDelegate?
+    
+    init(items: [String], delegate: PickerViewHandlerDelegate? = nil) {
+        self.items = items
+        self.delegate = delegate
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return items.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return items[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedItem = items[row]
+        delegate?.didSelectITem(self, selectedITem: selectedItem)
+    }
+}
