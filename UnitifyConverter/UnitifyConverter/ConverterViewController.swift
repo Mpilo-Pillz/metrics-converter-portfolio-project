dc9b255ba033637fc4fa4c8239b3a2f722a6a0e5
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-03-05 19:53:23 +0200

    showing result on screen although incorrect as it is the other way around

--- UnitifyConverter/UnitifyConverter/ConverterViewController.swift
+++ UnitifyConverter/UnitifyConverter/ConverterViewController.swift
@@ -10,20 +10,27 @@ import UIKit
 class ConverterViewController: UIViewController  {
     
     let measurementTypes = ["Mass", "Length", "Consumption"]
-    let units = ["Kilograms", "Pounds lb", ]
-    var unitsObj = [Unit(name: "Kilograms", conversionFactor: 10.0), Unit(name: "Pounds lb", conversionFactor: 100.0)]
+
+//    var units = [Unit(name: "Kilograms kg", conversionFactor: 1.0, unitType: UnitType.mass), Unit(name: "Pounds lb", conversionFactor: 2.20462, unitType: UnitType.mass), Unit(name: "Grams g", conversionFactor: 0.001, unitType: UnitType.mass)]
+//    453.592
     
+    var units = [
+        Unit(name: "Kilograms kg", conversionFactor: 1.0, unitType: .mass), // Base unit for mass
+        Unit(name: "Pounds lb", conversionFactor: 0.453592, unitType: .mass), // Pounds to kg
+        Unit(name: "Grams g", conversionFactor: 0.001, unitType: .mass) // Grams to kg
+    ]
     
-    var selectedFromUnit: String? //Unit?
-    var selectedToUnit: String? //Unit?
+    var selectedFromUnit: Unit?
+    var selectedToUnit: Unit?
     
+    @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var fromTextField: UITextField!
     @IBOutlet weak var measurementTypesPicker: UIPickerView!
     @IBOutlet weak var fromUnitPicker: UIPickerView!
     @IBOutlet weak var toUnitPicker: UIPickerView!
     
-    var measurementTypePickerviewHandler: PickerViewHandler!
-    var fromUitPickerViewHandler: PickerViewHandler!
+    var measurementTypePickerViewHandler: PickerViewHandler!
+    var fromUnitPickerViewHandler: PickerViewHandler!
     var toUnitPickerViewHandler: PickerViewHandler!
     
     override func viewDidLoad() {
@@ -36,17 +43,16 @@ class ConverterViewController: UIViewController  {
     }
     
     private func configurePickerViews() {
-        measurementTypePickerviewHandler = PickerViewHandler(items: measurementTypes, delegate: self)
-        fromUitPickerViewHandler = PickerViewHandler(items: units, delegate: self)
-        toUnitPickerViewHandler = PickerViewHandler(items: units, delegate: self)
-//        fromUitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
-//        toUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
+        measurementTypePickerViewHandler = PickerViewHandler(items: measurementTypes, delegate: self)
+        
+        fromUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
+        toUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
         
         
-        measurementTypesPicker.dataSource = measurementTypePickerviewHandler
-        measurementTypesPicker.delegate = measurementTypePickerviewHandler
-        fromUnitPicker.delegate = fromUitPickerViewHandler
-        fromUnitPicker.dataSource = fromUitPickerViewHandler
+        measurementTypesPicker.dataSource = measurementTypePickerViewHandler
+        measurementTypesPicker.delegate = measurementTypePickerViewHandler
+        fromUnitPicker.delegate = fromUnitPickerViewHandler
+        fromUnitPicker.dataSource = fromUnitPickerViewHandler
         toUnitPicker.delegate = toUnitPickerViewHandler
         toUnitPicker.dataSource = toUnitPickerViewHandler
     }
@@ -65,10 +71,16 @@ class ConverterViewController: UIViewController  {
         print("-->\(fromUnit)")
         print("-->\(toUnit)")
         
-//        let converter = Converter(fromUnit: fromUnit, toUnit: toUnit)
-        //        let result = converter.convert(value: fromValue)
+        let converter = Converter(fromUnit: fromUnit, toUnit: toUnit)
+                let result = converter.convert(value: fromValue)
         
-        //        outputTextField.text = "\(result)"
+//        resultLabel.text = "\(result)"
+        
+        guard let value = result else {
+            resultLabel.text = "Default Value or Leave Empty"
+            return
+        }
+        resultLabel.text = "\(value)"
         
     }
 }
@@ -76,13 +88,12 @@ class ConverterViewController: UIViewController  {
 extension ConverterViewController: PickerViewHandlerDelegate {
     func didSelectItem(_ handler: PickerViewHandler, selectedItem: String) {
                 
-        if handler === measurementTypePickerviewHandler {
+        if handler === measurementTypePickerViewHandler {
                    print("TODO filterList")
-               } else if handler === fromUitPickerViewHandler {
-                   selectedFromUnit = selectedItem
+               } else if handler === fromUnitPickerViewHandler {
+                   selectedFromUnit = units.first {$0.name == selectedItem}
                } else if handler === toUnitPickerViewHandler {
-                   
-                   selectedToUnit = selectedItem
+                   selectedToUnit = units.first {$0.name == selectedItem}
                }
     }
 }
