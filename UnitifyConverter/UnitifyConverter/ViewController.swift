5d0564407adfe043ddcd7c7bf7ab24f890f9c5e4
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-03-06 20:11:02 +0200

    further annalyzing the mass calulations

--- UnitifyConverter/UnitifyConverter/ViewController.swift
+++ UnitifyConverter/UnitifyConverter/ViewController.swift
@@ -16,11 +16,30 @@ class ViewController: UIViewController {
         kilometersPerLiter(from: 15.1)
         print("[----------------------------]")
         convertMilesToKm(miles: 2)
+        print("[-------------kg->pound---------------]")
+        convertKilogramsToPounds(kilograms: 65)
+        convertKilogramsToPounds2(kilograms: 65)
+        convertPoundsToKilograms2(pounds: 145)
     }
     
     func convertPoundsToKilograms(pounds: Double) {
         let kilograms = pounds * 0.453592
         print("kilograms - \(kilograms)")
+    }
+    
+    func convertKilogramsToPounds(kilograms: Double) {
+        let pounds = kilograms / 0.453592
+        print("pounds - \(pounds)")
+    }
+    
+    func convertPoundsToKilograms2(pounds: Double) {
+        let kilograms = pounds / 2.205
+        print("kilograms2 - \(kilograms)")
+    }
+    
+    func convertKilogramsToPounds2(kilograms: Double) {
+        let pounds = kilograms * 2.205
+        print("pounds2 - \(pounds)")
     }
     
 //    7.1l/100 -> ?liters / km
