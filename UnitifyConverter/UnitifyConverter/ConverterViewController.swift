00b01701dadc596ebdee656c5c77965a8015288f
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-03-10 13:19:02 +0200

    using the enum to populate the mass type

--- UnitifyConverter/UnitifyConverter/ConverterViewController.swift
+++ UnitifyConverter/UnitifyConverter/ConverterViewController.swift
@@ -18,7 +18,15 @@ class ConverterViewController: UIViewController  {
         Unit(name: "Grams g", conversionFactor: 0.001, unitType: .mass) // Grams to kg
     ]
     
+    let conversions: [UnitType: Conversion] = [
+        UnitType.mass: Conversion(units: ["kg", "lb", "g"]),
+        UnitType.distance: Conversion(units: ["miles", "km", "m"]),
+        UnitType.length: Conversion(units: ["m", "ft", "cm"]),
+        UnitType.fuelConsumption: Conversion(units: ["km/l", "l/100km",])
+    ]
     
+    
+    var selectedUnitType: UnitType = .mass
     
     var selectedFromUnit: Unit?
     var selectedToUnit: Unit?
@@ -43,10 +51,17 @@ class ConverterViewController: UIViewController  {
     }
     
     private func configurePickerViews() {
-        measurementTypePickerViewHandler = PickerViewHandler(items: massConverter.measurementUnits, delegate: self)
         
-        fromUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
-        toUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
+        measurementTypePickerViewHandler = PickerViewHandler(items: UnitType.allCases.map { $0.rawValue }, delegate: self)
+        
+        fromUnitPickerViewHandler = PickerViewHandler(items: [], delegate: self)
+        toUnitPickerViewHandler = PickerViewHandler(items: [], delegate: self)
+        
+        
+//        measurementTypePickerViewHandler = PickerViewHandler(items: massConverter.measurementUnits, delegate: self)
+        
+//        fromUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
+//        toUnitPickerViewHandler = PickerViewHandler(items: getStringsFromMap(from: units), delegate: self)
         
         
         measurementTypesPicker.dataSource = measurementTypePickerViewHandler
@@ -55,10 +70,17 @@ class ConverterViewController: UIViewController  {
         fromUnitPicker.dataSource = fromUnitPickerViewHandler
         toUnitPicker.delegate = toUnitPickerViewHandler
         toUnitPicker.dataSource = toUnitPickerViewHandler
+        
+        
+        
+        measurementTypesPicker.selectRow(0, inComponent: 0, animated: false)
+        
+//        fromUnitPicker.pickerView(unitTypePicker, didSelectRow: 0, inComponent: 0)
+        
     }
     
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
-       
+        
         guard let fromValue = Double(fromTextField.text ?? ""),
               let fromUnit = selectedFromUnit,
               let toUnit = selectedToUnit
@@ -68,7 +90,7 @@ class ConverterViewController: UIViewController  {
         }
         
         let result = massConverter.convert(value: fromValue, fromUnit: fromUnit, toUnit: toUnit)
-                
+        
         guard let value = result else {
             resultLabel.text = "Default Value or Leave Empty"
             return
@@ -80,13 +102,32 @@ class ConverterViewController: UIViewController  {
 
 extension ConverterViewController: PickerViewHandlerDelegate {
     func didSelectItem(_ handler: PickerViewHandler, selectedItem: String) {
-                
+        
         if handler === measurementTypePickerViewHandler {
-                   print("TODO filterList")
+                   // User selected a unit type, update the units picker
+                   if let unitType = UnitType(rawValue: selectedItem) {
+                       measurementTypePickerViewHandler?.items = conversions[unitType]?.units ?? []
+                       fromUnitPicker.reloadAllComponents()
+                       toUnitPicker.reloadAllComponents()
+                   }
                } else if handler === fromUnitPickerViewHandler {
-                   selectedFromUnit = units.first {$0.name == selectedItem}
-               } else if handler === toUnitPickerViewHandler {
-                   selectedToUnit = units.first {$0.name == selectedItem}
+                   
+//                   selectedFromUnit = units.first {$0.name == selectedItem}
                }
+        else if handler === toUnitPickerViewHandler {
+//            selectedToUnit = units.first {$0.name == selectedItem}
+        }
+
+        
+        
+//         if handler === measurementTypePickerViewHandler {
+// //            selectedUnitType = UnitType.allCases[row]
+// //            toUnitPicker.reloadAllComponents()
+// //            fromUnitPicker.reloadAllComponents()
+//         } else if handler === fromUnitPickerViewHandler {
+//             selectedFromUnit = units.first {$0.name == selectedItem}
+//         } else if handler === toUnitPickerViewHandler {
+//             selectedToUnit = units.first {$0.name == selectedItem}
+//         }
     }
 }
